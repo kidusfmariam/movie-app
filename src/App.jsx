0a7dd3ed231fe81f754105aac2e9c51f20d534cd
kidusfmariam <99491498+kidusfmariam@users.noreply.github.com>
2023-01-16 14:34:30 -0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+
+function App() {
+  return (
+    <div className="App">
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.cjs
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+      "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    colors : {
+      transparent: 'transparent',
+      black: '#000000',
+      white: '#ffffff',
+      pink: '#F20746',
+      blue: '#235789',
+      green: '#0DAB76'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
